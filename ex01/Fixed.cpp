fbe948539a4f5614557a8b88a178041ecedf45ac
arthurpnl <arthurpnlcontact1@gmail.com>
2026-02-03 11:30:53 +0100

    ex01

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,47 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    std::cout << "Default constructor called" << std::endl;
+    this->value = 0;
+}
+
+Fixed::Fixed (int const n)
+{
+    std::cout << "Int constructor called" << std::endl;
+    this->value = n * (1 << this->fractionalBits);
+}
+
+Fixed::Fixed (const float nb)
+{
+    std::cout << "Float constructor called" << std::endl;
+    this->value = nb * (1 << this->fractionalBits);
+}
+
+Fixed &Fixed::operator=(const Fixed &toAssign)
+{
+    this->value = toAssign.value;
+    std::cout << "Copy assignment operator called" << std::endl;
+    return (*this);
+}
+
+float   Fixed::toFloat(void) const
+{
+    return (float)this->value / (1 << this->fractionalBits);
+}
+
+int Fixed::toInt (void) const
+{
+    return this->value >> this->fractionalBits;
+}
+
+std::ostream &operator<<(std::ostream &o, const Fixed &fixed)
+{
+    o << fixed.toFloat();
+    return o;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <string>
+#include <iostream>
+
+class Fixed
+{
+    private:
+        int value;
+        const int static  fractionalBits = 8;
+
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(const int tmp);
+        Fixed (const float nb);
+        Fixed &operator=(const Fixed &toAssign);
+        float   toFloat(void) const;
+        int toInt (void) const;
+};
+        std::ostream &operator<<(std::ostream &o, const Fixed &fixed);
+
+#endif

9a5d78516527c00594bdccbc04ce39157fa5254e
Arthur <arthurpnlcontact1@gmail.com>
2026-03-31 12:52:22 +0200

    fixed main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include "Fixed.hpp"
 
-#include <iostream>
-#include "Fixed.hpp"
-
 int main( void ) 
 {
-Fixed a;
-Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-std::cout << a << std::endl;
-std::cout << ++a << std::endl;
-std::cout << a << std::endl;
-std::cout << a++ << std::endl;
-std::cout << a << std::endl;
-std::cout << b << std::endl;
-std::cout << Fixed::max( a, b ) << std::endl;
-r
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+}
